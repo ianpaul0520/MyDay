3bcd0e2a3e03267c8261b52dee159ae04995cb56
Christian Paul <ianpaul0520@gmail.com>
2020-03-13 14:14:36 +0800

    RE-ui'ing is 20%

---
+++ main.js
@@ -0,0 +1,19 @@
+// let getTheMidName = function() {
+//     return 
+// }
+
+// function callMe(fn) {
+
+// }
+// callMe(solid)
+
+function test(n) {
+    if(n > 2) {
+        test(n-1);
+        console.log('a');
+        console.log('b,');
+        if (n == 100) {return}
+    }
+    
+} 
+    test(5);
