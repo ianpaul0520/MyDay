83337d732bf89ec597b52eb1306ca0fb92be0d2e
Christian Paul <ianpaul0520@gmail.com>
2020-03-14 16:56:09 +0800

    adding level class to navigation section

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+let loginBtn = document.getElementById("login-button");
+let display = document.getElementById("display");
 const getNumber = 0;
 const specifiedValue = 140;
-let aDigit = Math.floor(Math.random() * Math.floor(20));
-console.log(aDigit);
-if (aDigit > specifiedValue) {
-}
+
+loginBtn.addEventListener("click", function() {
+  let lDigit = Math.floor(Math.random() * Math.floor(20));
+  let rDigit = Math.floor(Math.random() * Math.floor(21));
+  if (lDigit > specifiedValue) {
+    console.log(lDigit);
+  }
+  display.innerHTML = `LEFT:${lDigit} & RIGHT:${rDigit}`;
+});
 
 function tooHigh(n) {
   return n / 2;
